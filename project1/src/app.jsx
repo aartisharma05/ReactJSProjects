b93a9287e9d3094e6a2090f0f2f45bbe8c41da6b
AARTI SHARMA <120464099+aartisharma05@users.noreply.github.com>
2023-09-27 20:15:02 +0530

    app.jsx

---
+++ project1/src/app.jsx
@@ -0,0 +1,25 @@
+
+const App =()=> {
+
+  return (
+    <>
+  
+      <nav>
+        <div className="logo">
+          <img src="/images/brand_logo.png" alt="logo" />
+        </div>
+        <ul>
+          <li href="#">MENU</li>
+          <li href="#">LOCATION</li>
+          <li href="#">ABOUT</li>
+          <li href="#">CONTACT</li>
+        </ul>
+        <button>login</button> 
+        
+      </nav>
+   
+    </>
+  )
+}
+
+export default App
