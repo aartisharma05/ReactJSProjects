249ca3678fc3f188aa20ff8f380fab2a3c6ef877
AARTI SHARMA <120464099+aartisharma05@users.noreply.github.com>
2023-09-28 03:21:24 +0530

    Add files via upload

---
+++ project1/src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+import './App.css';
+import Nav from './components/nav'
+import HeroSection from './components/Hero';
+
+const App =()=> {
+
+  return (
+    <>
+  
+      <Nav/>
+     <HeroSection/>
+    </>
+  )
+}
+
+export default App
