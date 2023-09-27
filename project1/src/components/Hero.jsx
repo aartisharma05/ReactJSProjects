23cc0b223badd3c6c2eeca43b6cb1f0f5bfa0420
AARTI SHARMA <120464099+aartisharma05@users.noreply.github.com>
2023-09-28 03:22:53 +0530

    Hero.jsx

---
+++ project1/src/components/Hero.jsx
@@ -0,0 +1,38 @@
+const HeroSection = () =>{
+  return <>
+  
+  <main className="hero container">
+    <div className="hero-content">
+     <h1>YOUR FEET DESERVE THE BEST</h1>
+     <p>YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP YOU 
+        WITH OUR SHOES. YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP YOU 
+        WITH OUR SHOES.
+     </p>
+    
+
+  <div className="hero-btn">
+    <button>Shop Now</button>
+    <button className="cat-btn">Category</button>
+
+  </div>
+
+  <div className="shopping">
+    <p>Also available on</p>
+    <div className="brand-icons">
+    <img className="amz" src="images/amazon.png" alt="amazon"/>
+    <img className="flp" src="images/flipkart.png" alt="flipkart"/>
+    </div>
+  </div>
+  </div>
+
+  <div className="hero-image">
+       <img src="images/hero-img.png" alt="hero-img-logo"/>
+ 
+  </div>
+  </main>
+  
+  
+  </>
+}
+
+export default HeroSection;
