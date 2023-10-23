67676a34d7ac6be7b449937f560b66ff07db5432
AARTI SHARMA <aartisharma.official1@gmail.com>
2023-10-24 00:00:10 +0530

    IndexPage.js

---
+++ Project3/client/src/pages/IndexPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Post from '../Post';
+
+const IndexPage = () => {
+  return (
+    <>
+      <Post />
+      <Post />
+      <Post />
+    </>
+  );
+}
+
+export default IndexPage;
