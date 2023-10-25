30604151a87ff948b33c2a853653c006eec4861e
AARTI SHARMA <aartisharma.official1@gmail.com>
2023-10-25 23:24:55 +0530

    API.js

---
+++ Project5/src/API.js
@@ -0,0 +1,28 @@
+import { SEARCH_BASE_URL, POPULAR_BASE_URL, API_URL, API_KEY } from "./config";
+
+const defaultConfig = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+
+const apiSettings = {
+  fetchMovies: async (searchTerm, page) => {
+    const endpoint = searchTerm
+      ? `${SEARCH_BASE_URL}${searchTerm}&page=${page}`
+      : `${POPULAR_BASE_URL}&page=${page}`;
+    return await (await fetch(endpoint)).json();
+  },
+  fetchMovie: async (movieId) => {
+    const endpoint = `${API_URL}movie/${movieId}?api_key=${API_KEY}`;
+    return await (await fetch(endpoint)).json();
+  },
+  fetchCredits: async (movieId) => {
+    const creditsEndpoint = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`;
+    return await (await fetch(creditsEndpoint)).json();
+  },
+  // Bin
+};
+
+export default apiSettings;
