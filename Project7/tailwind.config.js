222c316f5b9420f5d9569270a3a86ac29cbf2271
AARTI SHARMA <aartisharma.official1@gmail.com>
2023-11-14 23:21:03 +0530

    tailwind.config.js

--- Project7/tailwind.config.js
+++ Project7/tailwind.config.js
@@ -3,7 +3,25 @@ module.exports = {
   content: ["public/*.{html,js}"],
   theme: {
     extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+      screens: {
+        sm: "600px",
+        md: "728px",
+        lg: "984px",
+        xl: "1240px",
+        "2xl": "1496px",
+      },
+    },
   },
   plugins: [],
-}
+};
+
 
